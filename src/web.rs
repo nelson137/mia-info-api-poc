e724c927aa6ff68479232fef17f2d2338299f97b
Nelson Earle <nelson.earle137@gmail.com>
2025-02-02 17:24:39 -0600

    replace `metrics-exporter-prometheus` with `axum-prometheus`

--- src/web.rs
+++ src/web.rs
@@ -1,14 +1,14 @@
-use axum::{Router, middleware, routing::get};
+use axum::{Router, routing::get};
+use axum_prometheus::PrometheusMetricLayer;
 
-mod metrics;
 mod routes_health;
 mod routes_hello;
 
 pub(crate) fn router() -> Router {
-    let metric_handle = metrics::setup_metrics_recorder();
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
     Router::new()
         .merge(routes_health::routes())
         .merge(routes_hello::routes())
         .route("/metrics", get(async move || metric_handle.render()))
-        .route_layer(middleware::from_fn(metrics::mw_track_metrics))
+        .layer(prometheus_layer)
 }
