20bfa87e253aa46afadec8411a86227a5262b611
Nelson Earle <nelson.earle137@gmail.com>
2025-02-03 12:26:40 -0600

    fix: bind to `0.0.0.0`

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,17 @@
 
 mod web;
 
+#[cfg(not(feature = "listen_public"))]
+const BIND_ADDR: &str = "localhost:8080";
+
+#[cfg(feature = "listen_public")]
+const BIND_ADDR: &str = "0.0.0.0:8080";
+
 #[tokio::main]
 async fn main() {
     let routes = web::router();
 
-    let listener = tokio::net::TcpListener::bind("localhost:8080")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind(BIND_ADDR).await.unwrap();
     eprintln!("Listening on {:?}", listener.local_addr().unwrap());
 
     axum::serve(listener, routes.into_make_service())
