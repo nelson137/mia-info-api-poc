8976c9697b505c54e2ddf4ab840198962349d0b7
Nelson Earle <nelson.earle137@gmail.com>
2025-02-02 18:17:34 -0600

    ignore `/self`, `/ready`, and `/metrics` from metrics

--- src/web.rs
+++ src/web.rs
@@ -1,11 +1,16 @@
 use axum::{Router, routing::get};
-use axum_prometheus::PrometheusMetricLayer;
+use axum_prometheus::PrometheusMetricLayerBuilder;
 
 mod routes_health;
 mod routes_hello;
 
 pub(crate) fn router() -> Router {
-    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayerBuilder::new()
+        .with_ignore_pattern("/self")
+        .with_ignore_pattern("/ready")
+        .with_ignore_pattern("/metrics")
+        .with_default_metrics()
+        .build_pair();
     Router::new()
         .merge(routes_health::routes())
         .merge(routes_hello::routes())
