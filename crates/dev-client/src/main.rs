498035af4d3ef4bd36c458bdf5b83e3fd8db5a63
Nelson Earle <nelson.earle137@gmail.com>
2025-02-05 10:15:07 -0600

    reimpl dev client with reqwest

--- crates/dev-client/src/main.rs
+++ crates/dev-client/src/main.rs
@@ -1,25 +1,66 @@
+use std::borrow::Cow;
+
 use anyhow::Result;
+use kitty_image::{Action, ActionPut, ActionTransmission, Command};
+use reqwest::{IntoUrl, Url, blocking};
 
-#[tokio::main]
-async fn main() -> Result<()> {
-    let client = httpc_test::new_client("http://localhost:8080")?;
+struct DevClient {
+    inner: blocking::Client,
+    base_url: Option<Url>,
+}
 
-    while !client
-        .do_get("/ready")
-        .await
-        .map(|r| r.status().is_success())
-        .unwrap_or(false)
-    {
-        tokio::time::sleep(std::time::Duration::from_millis(100)).await;
+impl DevClient {
+    fn new() -> Self {
+        Self {
+            inner: blocking::Client::new(),
+            base_url: None,
+        }
     }
 
-    client.do_get("/ready").await?.print().await?;
+    fn base_url(mut self, into_url: impl IntoUrl) -> Result<Self> {
+        self.base_url = Some(into_url.into_url()?);
+        Ok(self)
+    }
 
-    client
-        .do_get("/deployment/moa-dev/myqueue-contacts/badge")
-        .await?
-        .print()
-        .await?;
+    fn get(&self, partial_url: &str) -> Result<blocking::Response> {
+        let url = Url::options()
+            .base_url(self.base_url.as_ref())
+            .parse(partial_url)?;
+        Ok(self.inner.get(url).send()?)
+    }
+}
+
+fn main() -> Result<()> {
+    let client = DevClient::new().base_url("http://localhost:8080")?;
+
+    while !client
+        .get("/ready")
+        .map(|res| res.status().is_success())
+        .unwrap_or(false)
+    {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+    }
+
+    let badge_response = client.get("/deployment/moa-dev/myqueue-contacts/badge")?;
+    let badge_bytes = badge_response.bytes()?.into_iter().collect::<Vec<_>>();
+
+    let path = format!("{}/../../target/badge.png", std::env!("CARGO_MANIFEST_DIR"));
+    std::fs::write(path, &badge_bytes)?;
+
+    let mut cmd = Command::new(Action::TransmitAndDisplay(
+        ActionTransmission {
+            format: kitty_image::Format::Png,
+            medium: kitty_image::Medium::Direct,
+            ..Default::default()
+        },
+        ActionPut {
+            move_cursor: true,
+            ..Default::default()
+        },
+    ));
+    cmd.payload = Cow::Borrowed(&badge_bytes);
+    let cmd = kitty_image::WrappedCommand::new(cmd);
+    println!("{cmd}");
 
     Ok(())
 }
